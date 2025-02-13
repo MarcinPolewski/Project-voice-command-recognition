02911b0eaf973f9a98ed33d07523d54ac0539ddf
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-02-13 21:11:49 +0100

    create blueprint for dataset wrapper class

---
+++ dataset-wrapper.py
@@ -0,0 +1,31 @@
+from torch.utils.data import Dataset
+import pandas as pd
+import torchaudio
+
+
+class CommandsTrainDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, item_idx):
+        pass
+
+    def _preproces_sample(self, sample):
+        pass
+
+
+class CommandsTestDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, item_idx):
+        pass
+
+    def _preproces_sample(self, sample):
+        pass
