bb0027e4c67735135933ed13efcb27bc5776c41e
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-02-14 09:20:33 +0100

    implemented file responsible for creating training sample list

---
+++ constants.py
@@ -0,0 +1,37 @@
+TRAIN_LIST_PATH = "./train/train_list.txt"
+VALIDATE_LIST_PATH = "./train/validation_list.txt"
+TESTING_LIST_PATH = "./train/testing_list.txt"
+AUDIO_BASE_PATH = "./train/audio/"
+
+CLASS_MAPPINGS = [
+    "bed",
+    "bird",
+    "cat",
+    "dog",
+    "down",
+    "eight",
+    "five",
+    "four",
+    "go",
+    "happy",
+    "house",
+    "left",
+    "marvin",
+    "nine",
+    "no",
+    "off",
+    "on",
+    "one",
+    "right",
+    "seven",
+    "sheila",
+    "six",
+    "stop",
+    "three",
+    "tree",
+    "two",
+    "up",
+    "wow",
+    "yes",
+    "zero",
+]
