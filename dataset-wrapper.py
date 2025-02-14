5caa852edfd54223e59832d9b1d1ca10106e05ca
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-02-14 08:24:55 +0100

    small update

--- dataset-wrapper.py
+++ dataset-wrapper.py
@@ -3,8 +3,13 @@ import pandas as pd
 import torchaudio
 
 
-class CommandsTrainDataset(Dataset):
-    def __init__(self):
+class CommandsDataset(Dataset):
+    def __init__(
+        self,
+        path_to_paths,  # path to txt file fith paths of audio files for test/train/validation
+    ):
+        self.data = self._load_data_from_file(path_to_paths)
+
         pass
 
     def __len__(self):
@@ -15,6 +20,17 @@ class CommandsTrainDataset(Dataset):
 
     def _preproces_sample(self, sample):
         pass
+
+    def _load_data_from_file(self, path_to_paths):
+        with open(path_to_paths, "r") as file:
+            for line in file:
+                commandClass = line.split("/")[0]
+                path = base_path + line
+
+
+class CommandsTrainDataset(CommandsDataset):
+    def __init__(self):
+        super().__init__("./train/")
 
 
 class CommandsTestDataset(Dataset):
