2b3e81bdc627dba0b7cd7c55dd6368aeff8ba5b0
Michał Gryglicki <michal.gryglicki2.stud@pw.edu.pl>
2025-02-15 23:28:06 +0100

    replace CNN_2d with CNN_1d in test.py and integrate CommandsTestDataset for model evaluation

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import torchaudio
 import torch
 from convoluted_network_2d import CNN_2d
-from dataset_wrapper import CommandsTrainDataset
+from convoluted_network_1d import CNN_1d
+from dataset_wrapper import CommandsTestDataset
 from constants import CLASS_MAPPINGS
 
 
@@ -18,29 +19,35 @@ def predict(model, input, target):
 
 if __name__ == "__main__":
     # load back the model
-    model = CNN_2d()
-    state_dict = torch.load("backup/cnn_2d_9")
+    model = CNN_1d()
+    state_dict = torch.load("backup/cnn_1d_9")
     model.load_state_dict(state_dict)
 
     target_sampling_rate = 16000
     target_number_of_samples = 16000
-    transformation = torchaudio.transforms.MelSpectrogram(
-        sample_rate=target_sampling_rate, n_fft=1024, hop_length=512, n_mels=64
-    )
+    transformation = None
 
-    validation_data = CommandsTrainDataset(
+    testing_data = CommandsTestDataset(
         device="cpu",
         target_sampling_rate=target_sampling_rate,
         target_number_of_samples=target_number_of_samples,
         transformation=transformation,
     )
 
-    input, target = validation_data[
-        0
-    ]  # here input has 3 dimensions -> [number_of_chanels, frequency_axis, time_axis]
-    input.unsqueeze_(
-        0
-    )  # addin another dimension - model expexts [batch_size, number_of_chanels, ...]
-    # 0 is the index, where we add new dimension
+    # input, target = validation_data[
+    #     0
+    # ]  # here input has 3 dimensions -> [number_of_chanels, frequency_axis, time_axis]
+    # input.unsqueeze_(
+    #     0
+    # )  # addin another dimension - model expexts [batch_size, number_of_chanels, ...]
+    # # 0 is the index, where we add new dimension
 
-    print(predict(model, input, target))
+    ct = 0
+    for input, target in testing_data:
+        input.unsqueeze_(0)
+
+        predited, expected = predict(model, input, target)
+        print(f'predicted: {predited}, expected: {expected}')
+        ct += predited == expected
+
+    print(f'Accuraccy: {ct / len(testing_data)}')
