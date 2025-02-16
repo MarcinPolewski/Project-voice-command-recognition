ce20da97aea071672c195208736df690a19aadfa
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-02-16 19:06:09 +0100

    implemented dynamic selection of selection classes

--- dataset_wrapper.py
+++ dataset_wrapper.py
@@ -4,6 +4,8 @@ import torchaudio
 from train_list_generator import Train_List_Generator
 import constants
 import torch
+import hashlib
+import re
 
 
 class CommandsDataset(Dataset):
@@ -12,7 +14,7 @@ class CommandsDataset(Dataset):
 
     def __init__(
         self,
-        path_to_paths,  # path to txt file fith paths of audio files for test/train/validation - for instance ./train/validation_list.txt
+        dataset_group,  # path to txt file fith paths of audio files for test/train/validation - for instance ./train/validation_list.txt
         device,
         target_sampling_rate,  # all returns samples will have this sr
         target_number_of_samples,  # how many samples should target recording have
@@ -22,8 +24,12 @@ class CommandsDataset(Dataset):
         self.target_sampling_rate = target_sampling_rate
         self.target_number_of_samples = target_number_of_samples
         self.transformation = transformation
+        self.validation_percentage = constants.validation_percentage
+        self.test_percentage = constants.test_percentage
+
         Train_List_Generator.generate()
-        self.data = self._load_sound_list(path_to_paths)
+        self.dataset_group = dataset_group
+        self.data = self._load_sound_list()
 
     def __len__(self):
         return len(self.data)
@@ -104,25 +110,70 @@ class CommandsDataset(Dataset):
         sample = self._adjust_length(sample)
         return sample
 
-    def _load_sound_list(self, path_to_paths):
+    def _is_train_test_or_validate(self, filename):
         """
-        loads a file with audio samples(for training/testing/validation) to python object
+        assign this file to set in a way to reduce risk of train sample being resuded in testing etc
+        here where files are assigned only depens on filename and percentage values provided as arguments
+        also realted files(same command by the same actor will be assigned to the same set)
         """
+        base_name = os.path.basename(filename)
+
+        hash_name = re.sub(r'_nohash_.*$', '', base_name)
+
+        hash_name_hashed = hashlib.sha1(hash_name.encode("utf-8")).hexdigest()
+        percentage_hash = ((int(hash_name_hashed, 16) %
+                            (constants.MAX_NUM_WAVS_PER_CLASS + 1)) *
+                            (100.0 / constants.MAX_NUM_WAVS_PER_CLASS))
+        
+        if percentage_hash < self.validation_percentage:
+            return constants.DatasetGroup.VALIDATE
+        elif percentage_hash < (self.test_percentage + self.validation_percentage):
+            return constants.DatasetGroup.TEST
+        return constants.DatasetGroup.TRAIN
+
+    # def _load_sound_list(self, path_to_paths):
+    #     """
+    #     loads a file with audio samples(for training/testing/validation) to python object
+    #     """
+
+    #     data = []
+
+    #     with open(path_to_paths, "r") as file:
+    #         base_path = constants.AUDIO_BASE_PATH
+    #         for line in file:
+    #             sample_class = line.split("/")[0]
+    #             path = base_path + line
+    #             path = path.strip()
+
+    #             data_row = [path, sample_class]
+    #             data.append(data_row)
+
+
+
+    #     return data
+
+
+    def _load_sound_list(self):
 
         data = []
 
-        with open(path_to_paths, "r") as file:
-            base_path = constants.AUDIO_BASE_PATH
-            for line in file:
-                sample_class = line.split("/")[0]
-                path = base_path + line
-                path = path.strip()
+        base_path = constants.AUDIO_BASE_PATH
+        
+        for class_name in constants.CLASS_MAPPINGS:
+            class_folder_path = base_path + class_name
+            all_files_of_this_class = os.listdir(class_folder_path)
 
-                data_row = [path, sample_class]
-                data.append(data_row)
+            for filename in all_files_of_this_class: # file is just a file name
+                if self.dataset_group == self._is_train_test_or_validate(filename):
+                    full_filename = class_name + "/" + filename
+
+                    result_path = base_path + full_filename
+                    result_path.strip()
+
+                    data_row = [result_path, class_name]
+                    data.append(data_row)
 
         return data
-
 
 class CommandsTrainDataset(CommandsDataset):
     def __init__(
@@ -133,7 +184,7 @@ class CommandsTrainDataset(CommandsDataset):
         transformation,
     ):
         super().__init__(
-            "./train/train_list.txt",  # path to txt file fith paths of audio files for test/train/validation - for instance ./train/validation_list.txt
+            constants.DatasetGroup.TRAIN,  
             device,
             target_sampling_rate,  # all returns samples will have this sr
             target_number_of_samples,  # how many samples should target recording have
@@ -150,9 +201,38 @@ class CommandsTestDataset(CommandsDataset):
         transformation,
     ):
         super().__init__(
-            "./train/testing_list.txt",  # path to txt file fith paths of audio files for test/train/validation - for instance ./train/validation_list.txt
+            constants.DatasetGroup.TEST, 
             device,
             target_sampling_rate,  # all returns samples will have this sr
             target_number_of_samples,  # how many samples should target recording have
             transformation,
         )
+
+class CommandsValidateDataset(CommandsDataset):
+    def __init__(
+        self,
+        device,
+        target_sampling_rate,  # all returns samples will have this sr
+        target_number_of_samples,  # how many samples should target recording have
+        transformation,
+    ):
+        super().__init__(
+            constants.DatasetGroup.VALIDATE,  
+            device,
+            target_sampling_rate,  # all returns samples will have this sr
+            target_number_of_samples,  # how many samples should target recording have
+            transformation,
+        )
+
+if __name__ == "__main__":
+    trans  = torchaudio.transforms.MelSpectrogram(
+        sample_rate=16000, n_fft=1024, hop_length=512, n_mels=64
+    )
+    ds = CommandsTestDataset("cpu", 16000, 16000, trans)
+    print(len(ds))
+
+    ds = CommandsTrainDataset("cpu", 16000, 16000, trans)
+    print(len(ds))
+
+    ds = CommandsValidateDataset("cpu", 16000, 16000, trans)
+    print(len(ds))
